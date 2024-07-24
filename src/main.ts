ceaea9b5eabecd12c25f9911713a73c1b82979a6
김만규 <123226237+fierceCry@users.noreply.github.com>
2024-07-24 22:45:12 +0900

    feat: main.ts에 swagger Bearer 인증 추가
    Swagger 문서화에 Bearer 인증 구성을 main.ts 파일에 추가했습니다.
    .addBearerAuth 메서드를 사용하여 JWT 인증 스키마를 지원하도록 설정했습니다.

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
     .setVersion("1.0")
     .addTag("cats")
     .addServer('api/v1')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
