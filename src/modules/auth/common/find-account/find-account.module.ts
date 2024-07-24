a9bf218ea66eceb47a8c89b5e6c867bd7db6bf9d
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-07-24 21:31:25 +0900

    fix/find-account, sign-out modules refrence error resolve.

--- src/modules/auth/common/find-account/find-account.module.ts
+++ src/modules/auth/common/find-account/find-account.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { FindAccountController } from './find-account.controller';
-import { FindAccountService } from './find-account.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { EmailVerificationService } from "../email/email.service";
+
+import { FindAccountService } from "./find-account.service";
+import { FindAccountController } from "./find-account.controller";
+
+import { UsersEntity } from "src/entities/users.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   controllers: [FindAccountController],
-  providers: [FindAccountService]
+  providers: [FindAccountService, EmailVerificationService],
 })
 export class FindAccountModule {}
