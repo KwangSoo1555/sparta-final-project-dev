a3c9f29c883ab6d1058d5dd6ef0ccc98a7503b1e
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-08-25 10:35:51 +0900

    fix/resolve redis and smtp

--- src/database/redis/redis.module.ts
+++ src/database/redis/redis.module.ts
@@ -13,9 +13,11 @@ import { RedisPubsubService } from "./redis-pubsub.service";
         type: "single",
         options: {
           host: configService.get("REDIS_HOST"),
-          port: configService.get("REDIS_PORT"),
+          port: parseInt(configService.get("REDIS_PORT")),
+          username: configService.get("REDIS_USERNAME"),
           password: configService.get("REDIS_PASSWORD"),
         },
+        retryStrategy: (times) => Math.min(times * 50, 2000),
       }),
       inject: [ConfigService],
     }),
